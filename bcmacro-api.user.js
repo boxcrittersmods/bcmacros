669252f646c3189d3693e7fdd6b2478c3c563c5e
Tumble <tumblegamer@gmail.com>
2020-05-29 21:50:35 +0100

    update

--- bcmacro-api.user.js
+++ bcmacro-api.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BCMacro API
 // @namespace    http://discord.gg/G3PTYPy
-// @version      0.3.15.48
+// @version      0.3.16.49
 // @description  Adds Macro API
 // @author       TumbleGamer
 // @resource fontAwesome https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css
