0bf84483f9bf1ec16124c11b83af35ff7e3d4b89
Tumble <tumblegamer@gmail.com>
2020-10-16 02:44:07 +0100

    version

--- bcmacro-api.user.js
+++ bcmacro-api.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BCMacro API
 // @namespace    http://discord.gg/G3PTYPy
-// @version      0.6.7.89
+// @version      0.6.8.90
 // @description  Adds Buttons and Keybinds to Box Critters
 // @author       TumbleGamer
 // @resource fontAwesome https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css
