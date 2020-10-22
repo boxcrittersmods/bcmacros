8a2dc06f68d9c0dc14916128e44438ac61615b37
Tumble <tumblegamer@gmail.com>
2020-10-22 19:37:50 +0100

    critterguration

--- bcmacro-api.user.js
+++ bcmacro-api.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         BCMacro API
 // @namespace    http://discord.gg/G3PTYPy
-// @version      0.7.0.100
+// @version      0.7.0.101
 // @description  Adds Buttons and Keybinds to Box Critters
 // @author       TumbleGamer
 // @resource fontAwesome https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css
 // @require      https://github.com/tumble1999/mod-utils/raw/master/mod-utils.js
 // @require      https://github.com/tumble1999/popper/raw/master/popper.js
 // @require      https://github.com/SArpnt/ctrl-panel/raw/master/script.user.js
-// @require      https://github.com/tumble1999/critterguration/raw/master/critterguration.js
+// @require      https://github.com/tumble1999/critterguration/raw/master/critterguration.user.js
 // @match        https://boxcritters.com/play/
 // @match        https://boxcritters.com/play/?*
 // @match        https://boxcritters.com/play/#*
