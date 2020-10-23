8a0f3dd0e4669fa83f2d0c4871c688d24d6ee8f1
Tumble <tumblegamer@gmail.com>
2020-10-23 09:33:53 +0100

    no more settings macro

--- bcmacro-api.user.js
+++ bcmacro-api.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BCMacro API
 // @namespace    http://discord.gg/G3PTYPy
-// @version      0.7.6.106
+// @version      0.8.0.107
 // @description  Adds Buttons and Keybinds to Box Critters
 // @author       TumbleGamer
 // @require      https://kit.fontawesome.com/efb91a96ed.js
@@ -230,17 +230,6 @@
 
 		if (notice) sendNotice(notice, type || "warning");
 
-		if (settingsMacro.inaccessible()) {
-			sendNotice(`Please set an activation method for the settings macro.`, "danger");
-			BCMacros.settingsPage.getElementsByClassName("bcmSettingSave")[0].disabled = true;
-			//Todo: add disable closng to Critterguration
-			//BCMacros.modal.disableClosing();
-		} else {
-			BCMacros.settingsPage.getElementsByClassName("bcmSettingSave")[0].disabled = false;
-			//Todo: add disable closng to Critterguration
-			//BCMacros.modal.enableClosing();
-		}
-
 		for (let packId in packs) {
 			let pack = packs[packId];
 			let list = document.createElement("div");
@@ -424,20 +413,6 @@
 		return macroPack;
 	}
 
-	let me = createMacroPack("BCMacros");
-
-	let settingsMacro = me.createMacro({
-		name: "settings",
-		action: _ => Critterguration.openSettings(),
-		button: {
-			text: `
-			<svg width="1.5em" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="cog" class="svg-inline--fa fa-cog fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-			<path fill="currentColor" d="M487.4 315.7l-42.6-24.6c4.3-23.2 4.3-47 0-70.2l42.6-24.6c4.9-2.8 7.1-8.6 5.5-14-11.1-35.6-30-67.8-54.7-94.6-3.8-4.1-10-5.1-14.8-2.3L380.8 110c-17.9-15.4-38.5-27.3-60.8-35.1V25.8c0-5.6-3.9-10.5-9.4-11.7-36.7-8.2-74.3-7.8-109.2 0-5.5 1.2-9.4 6.1-9.4 11.7V75c-22.2 7.9-42.8 19.8-60.8 35.1L88.7 85.5c-4.9-2.8-11-1.9-14.8 2.3-24.7 26.7-43.6 58.9-54.7 94.6-1.7 5.4.6 11.2 5.5 14L67.3 221c-4.3 23.2-4.3 47 0 70.2l-42.6 24.6c-4.9 2.8-7.1 8.6-5.5 14 11.1 35.6 30 67.8 54.7 94.6 3.8 4.1 10 5.1 14.8 2.3l42.6-24.6c17.9 15.4 38.5 27.3 60.8 35.1v49.2c0 5.6 3.9 10.5 9.4 11.7 36.7 8.2 74.3 7.8 109.2 0 5.5-1.2 9.4-6.1 9.4-11.7v-49.2c22.2-7.9 42.8-19.8 60.8-35.1l42.6 24.6c4.9 2.8 11 1.9 14.8-2.3 24.7-26.7 43.6-58.9 54.7-94.6 1.5-5.5-.7-11.3-5.6-14.1zM256 336c-44.1 0-80-35.9-80-80s35.9-80 80-80 80 35.9 80 80-35.9 80-80 80z"></path>
-			</svg>`,
-			color: "primary"
-		},
-	});
-
 	let customMacros = createMacroPack("Custom");
 	// Setup custom macros
 
@@ -480,9 +455,6 @@
 				document.getElementById(`bcmSetting_${macro.id}`).firstElementChild.classList.remove("bg-success", "text-white");
 		}, false);
 	});
-
-	if (settingsMacro.inaccessible())
-		Critterguration.openSettings("bcmacros");
 
 	exportFunction(BCMacros, unsafeWindow, {
 		defineAs: "BCMacros",
